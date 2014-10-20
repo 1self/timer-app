a1b17e4da6b5708b1b6b5ed6e2e03a4f221a19fa
Douglas Vaz <douglas1992@gmail.com>
2014-10-20 15:41:23 +0530

    displaying a simple counter on activity tap.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,13 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $ionicModal) {
-    $scope.timerEvents = [
-        { title: 'Exercise' },
-        { title: 'Meditate' },
-        { title: 'Meetings' },
-        { title: 'Tooth brushing' },
-        { title: 'Sleeping' },
-        { title: 'Coding' }
+    $scope.activities = [
+        { title: 'Meditate', duration: 0},
+        { title: 'Exercise', duration: 0},
+        { title: 'Meetings', duration: 0},
+        { title: 'Tooth brushing', duration: 0},
+        { title: 'Sleeping', duration: 0},
+        { title: 'Coding', duration: 0}
     ];
     $scope.addTimerEvent = function(){
         $scope.addTimerEventModal.show();
@@ -22,6 +22,19 @@ angular.module('starter.controllers', [])
     $scope.closeNewTimerEvent = function(){
         $scope.addTimerEventModal.hide();
     };
+    $scope.toggleActivity = function(activity) {
+        if(!activity.interval) {
+            activity.interval = setInterval(function () {
+                activity.duration += 1;
+                $scope.$apply();
+            }, 1000);
+        } else {
+            clearInterval(activity.interval);
+            delete activity.interval;
+        }
+        
+        console.log(activity);
+    }
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
