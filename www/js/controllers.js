ea1ab161533bf9f42369b2224f798ff03091ea52
Douglas Vaz <douglas1992@gmail.com>
2014-10-21 17:07:39 +0530

    - added plugin to show toast on mobile

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,21 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $ionicModal, ActivityTimingService) {
+.controller('DashCtrl', function($scope, $ionicModal, $cordovaToast, ActivityTimingService) {
 	$scope.activities = ActivityTimingService.getAllActivities();
 
 	$scope.toggleActivity = function(activity) {
 		ActivityTimingService.toggleActivity(activity);
+		$scope.showToast();
 	};
+
+	$scope.showToast = function() {
+		$cordovaToast.show("Toast works!", 'long', 'bottom')
+			.then(function(success) {
+				console.log("The toast was shown");
+			}, function(error) {
+				console.log("The toast was not shown due to " + error);
+			});
+	}
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
