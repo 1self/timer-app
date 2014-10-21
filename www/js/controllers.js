f700d2e4409e65798322e7c2bfe0af11e6baa29c
Douglas Vaz <douglas1992@gmail.com>
2014-10-21 16:43:33 +0530

    - moved the logic to service. - using $interval instead of setInterval

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,37 +1,11 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $ionicModal, moment) {
-	$scope.activities = [{
-		title: 'Meditate'
-	}, {
-		title: 'Exercise'
-	}, {
-		title: 'Meetings'
-	}, {
-		title: 'Tooth brushing'
-	}, {
-		title: 'Sleeping'
-	}, {
-		title: 'Coding'
-	}];
+.controller('DashCtrl', function($scope, $ionicModal, ActivityTimingService) {
+	$scope.activities = ActivityTimingService.getAllActivities();
 
 	$scope.toggleActivity = function(activity) {
-		var updateActivityTime = function() {
-			var elapsedTime = moment.duration(moment().diff(activity.startDate));
-			activity.duration = moment.utc(elapsedTime.asMilliseconds());
-			$scope.$apply();
-		};
-		if (!activity.interval) {
-			activity.startDate = moment();
-			activity.interval = setInterval(updateActivityTime, 1000);
-		} else {
-			delete activity.startDate;
-			delete activity.duration;
-			clearInterval(activity.interval);
-			delete activity.interval;
-		}
-		console.log(activity);
-	}
+		ActivityTimingService.toggleActivity(activity);
+	};
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,18 +1,67 @@
 angular.module('starter.services', [])
 
-/**
- * A simple example service that returns some data.
- */
+.service('ActivityTimingService', function(moment, $interval) {
+  var activities = [{
+    title: 'Meditate'
+  }, {
+    title: 'Exercise'
+  }, {
+    title: 'Meetings'
+  }, {
+    title: 'Tooth brushing'
+  }, {
+    title: 'Sleeping'
+  }, {
+    title: 'Coding'
+  }];
+
+  var toggleActivity = function(activity) {
+    var updateActivityTime = function() {
+      var elapsedTime = moment.duration(moment().diff(activity.startDate));
+      activity.duration = moment.utc(elapsedTime.asMilliseconds());
+    };
+    if (!activity.interval) {
+      activity.startDate = moment();
+      updateActivityTime();
+      activity.interval = $interval(updateActivityTime, 1000);
+    } else {
+      delete activity.startDate;
+      delete activity.duration;
+      $interval.cancel(activity.interval);
+      delete activity.interval;
+    }
+    console.log(activity);
+    console.log("activities : " + JSON.stringify(activities));
+  };
+
+  var getAllActivities = function() {
+    return activities;
+  }
+
+  return {
+    toggleActivity: toggleActivity,
+    getAllActivities: getAllActivities
+  };
+
+})
+
 .factory('Friends', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
+  var friends = [{
+    id: 0,
+    name: 'Scruff McGruff'
+  }, {
+    id: 1,
+    name: 'G.I. Joe'
+  }, {
+    id: 2,
+    name: 'Miss Frizzle'
+  }, {
+    id: 3,
+    name: 'Ash Ketchum'
+  }];
 
   return {
     all: function() {
