d8fa57e98238061156e98deebf9d02283401802c
Devaroop <devaroop123@yahoo.co.in>
2014-11-05 16:23:16 +0530

    add ios7 status bar fix

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -30,7 +30,7 @@ angular.module('starter.controllers', [])
                 tags.actionTags.join(',') +
                 "/sum(duration)/daily/barchart";
 
-            var ref = window.open(uri, '_blank', 'location=yes,closebuttoncaption="Back To App"');
+            var ref = window.open(uri, '_blank', 'location=yes,closebuttoncaption=Back to App');
         };
 
 	$scope.showToast = function(message) {
