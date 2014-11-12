3a38a7504c3306ebf30130bd4974dcf8d3b54cf3
Douglas Vaz <douglas1992@gmail.com>
2014-11-12 18:08:47 +0530

    iOS fix

--- www/js/app.js
+++ www/js/app.js
@@ -33,6 +33,18 @@ angular.module('duration', ['ionic', 'ngCordova', 'duration.controllers', 'durat
             }else{
                 AuthenticationService.authenticate();
             }
+
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+
+                //Change this to false to return accessory bar 
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
         })
     })
 
